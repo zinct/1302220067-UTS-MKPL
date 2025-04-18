0dbad700a01d05da99d3f305ed16cd199172b297
Indra Mahesa <indramahesa128@gmail.com>
2025-04-18 14:13:44 +0700

    refactor readiblity dari code

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -2,7 +2,6 @@ package lib;
 
 public class TaxFunction {
 
-    // Penambahan konstanta untuk menggantikan magic number
     private static final int BASE_NON_TAXABLE = 54000000;
     private static final int MARRIAGE_ALLOWANCE = 4500000;
     private static final int CHILD_ALLOWANCE = 4500000;
@@ -17,36 +16,41 @@ public class TaxFunction {
      * Jika pegawai belum menikah dan belum punya anak maka penghasilan tidak kena pajaknya adalah Rp 54.000.000.
      * Jika pegawai sudah menikah maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000.
      * Jika pegawai sudah memiliki anak maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000 per anak sampai anak ketiga.
-     * 
      */
     
     public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
-        
-        int tax = 0;
-        
+
         if (numberOfMonthWorking > 12) {
             System.err.println("More than 12 month working per year");
         }
-        
-        if (numberOfChildren > 3) {
-            numberOfChildren = 3;
-        }
-        
-        if (isMarried) {
-            // 54000000 = BASE_NON_TAXABLE
-            // 4500000 = MARRIAGE_ALLOWANCE
-            // 1500000 * numberOfChildren = CHILD_ALLOWANCE * numberOfChildren (yang ditulis salah di logika awal)
-            tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-        }else {
-            tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
-        }
-        
-        if (tax < 0) {
-            return 0;
-        }else {
-            return tax;
-        }
-             
+
+        // Membatasi jumlah anak maksimal 3
+        numberOfChildren = Math.min(numberOfChildren, MAX_CHILDREN);
+
+        int annualIncome = calculateAnnualIncome(monthlySalary, otherMonthlyIncome, numberOfMonthWorking);
+        int nonTaxableIncome = calculateNonTaxableIncome(isMarried, numberOfChildren);
+
+        int tax = calculateTaxAmount(annualIncome, deductible, nonTaxableIncome);
+
+        return Math.max(tax, 0);
     }
-    
+
+    private static int calculateAnnualIncome(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking) {
+        return (monthlySalary + otherMonthlyIncome) * numberOfMonthWorking;
+    }
+
+    private static int calculateNonTaxableIncome(boolean isMarried, int numberOfChildren) {
+        int nonTaxableIncome = BASE_NON_TAXABLE;
+
+        if (isMarried) {
+            nonTaxableIncome += MARRIAGE_ALLOWANCE;
+            nonTaxableIncome += CHILD_ALLOWANCE * numberOfChildren;
+        }
+
+        return nonTaxableIncome;
+    }
+
+    private static int calculateTaxAmount(int annualIncome, int deductible, int nonTaxableIncome) {
+        return (int) Math.round(TAX_PERCENTAGE * (annualIncome - deductible - nonTaxableIncome));
+    }
 }
